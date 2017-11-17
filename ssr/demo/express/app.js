462b61569c2ebe0850e06bba1425133b2d61aae5
Andre Bandarra <andreban@google.com>
2017-11-17 15:47:59 +0000

    Adds SSR implementation to the demo
    Uses a configurable Node middleware to apply the transformations to AMP files.
    
    Change-Id: Id67dfe5aa658573387980ab2c59b56c52cef4c71

--- ssr/demo/express/app.js
+++ ssr/demo/express/app.js
@@ -16,14 +16,93 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const DEFAULT_PORT = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const ssr = require('../../index.js');
+ssr.setConfig({
+  transformers: [
+    'AddAmpLink',
+    'ServerSideRendering',
+    'RemoveAmpAttribute',
+    // needs to run after ServerSideRendering
+    'AmpBoilerplateTransformer',
+    // needs to run after ServerSideRendering
+    'ReorderHeadTransformer'
+  ]
 });
 
+// The transform middleware replaces the `res.write` method so that, instead of sending
+// the content to the network, it is accumulated in a buffer. `res.end` is also replaced
+// so that, when it is invoked, the buffered response is transformed with AMP-SSR and sent
+// to the network.
+const createTransformMiddleware = options => {
+  options.ampPath = options.ampPath || '/amp/';
+
+  return (req, res, next) => {
+    // This is a request for the original AMP. Allow the flow to continue normally.
+    if (req.url.startsWith(options.ampPath)) {
+      next();
+      return;
+    }
+
+    req.url = path.join(options.ampPath, req.url);
+
+    const chunks = [];
+
+    // We need to store the original versions of those methods, as we need to invoke
+    // them to finish the request correctly.
+    const originalEnd = res.end;
+    const originalWrite = res.write;
+
+    res.write = chunk => {
+      chunks.push(chunk);
+    };
+
+    res.end = chunk => {
+      // Replace methods with the original implementation.
+      res.write = originalWrite;
+      res.end = originalEnd;
+
+      if (chunk) {
+        // When an error (eg: 404) happens, express-static sends a string with
+        // the error message on this chunk. If that's the case,
+        // just pass forward the call to end.
+        if (typeof chunk === 'string') {
+          res.end(chunk);
+          return;
+        }
+        chunks.push(chunk);
+      }
+
+      // If end is called withouth any chunks, end the request.
+      if (chunks.length === 0) {
+        res.end();
+        return;
+      }
+
+      const body = Buffer.concat(chunks).toString('utf8');
+      const transformedBody =
+          ssr.transformHtml(body, {ampUrl: path.join(options.ampPath, req.originalUrl)});
+
+      res.send(transformedBody);
+    };
+
+    next();
+  };
+};
+
+// Setup the AMP-SSR Transformer and pass along the path to build the link tag.
+const transformMiddleware = createTransformMiddleware({});
+
+// It's important that the transformMiddleware is added BEFORE the static middleware.
+// This allows us to replace the parts needed before static handles the request.
+app.get('/*.html', transformMiddleware);
+
+const staticMiddelware = express.static(path.join(__dirname, '/public'));
+app.use(staticMiddelware);
+
+const DEFAULT_PORT = 3000;
 const port = process.env.PORT || DEFAULT_PORT;
 app.listen(port, () => {
   console.log('Example app listening on port 3000!');
