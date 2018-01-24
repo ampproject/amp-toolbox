2b5038461df99ea989cf7c96baadc7c95c8aa93b
Andre Bandarra <andreban@google.com>
2018-01-24 11:38:11 +0000

    cache-url
    Change-Id: Iabb74305134d6e8ae2a96683c7cf2eb34b0790ad

---
+++ cache-url/README.md
@@ -0,0 +1,6 @@
+# AMP-Toolbox Cache URL
+
+Translates an url from the origin to the AMP Cache URL format, according to the specification
+available in the [AMP documentation](https://developers.google.com/amp/cache/overview).
+
+## Usage
---
+++ cache-url/lib/AmpUrl.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+/**
+ * Translates canonical URLs into AMP Cache URLs, according to the specification available at
+ * https://developers.google.com/amp/cache/overview.
+ */
+class AmpUrl {
+  /**
+   * Translates the canonicalUrl to the AMP Cache equivalent, for a given AMP Cache.
+   * @param {string} canonicalUrl
+   */
+  cacheUrl(ampCache, canonicalUrl) {
+
+  }
+}
+
+/** @module AmpUrl */
+module.exports = AmpUrl;
