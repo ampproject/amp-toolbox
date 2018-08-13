0ae2913e6996c8b40d33ca7ebf51ba7ff280f3a4
Addie Achan <39060570+addieachan@users.noreply.github.com>
2018-08-13 16:06:55 -0400

    Added comments

--- optimizer/lib/transformers/BlurImagePlaceholders.js
+++ optimizer/lib/transformers/BlurImagePlaceholders.js
@@ -1,6 +1,28 @@
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 'use strict';
 var sizeOf = require('image-size');
 var jimp = require('jimp');
+
+/**
+ * Adds placeholders for all AMP images that are blurry versions of the
+ * corresponding original source. The blur will be displayed as the <amp-img> is
+ * loading, and
+ */
+
 class BlurImagePlaceholders {
 
   transform(tree, params) {
@@ -13,6 +35,7 @@ class BlurImagePlaceholders {
         let parent = node;
         ampImgPromises.push(this.addBitmap(tree, parent).then
         (imgChild => {
+            console.log("4");
             parent.appendChild(imgChild);
         }));
       }
@@ -43,6 +66,7 @@ class BlurImagePlaceholders {
     const bitMapDims = this.getBitmapDimensions(node, tree);
     return this.createBitmap(node, bitMapDims[0], bitMapDims[1]).then
     (dataURI => {
+      console.log("2");
       node.attribs.src = dataURI;
     });
   }
@@ -65,7 +89,11 @@ class BlurImagePlaceholders {
     return jimp.read(node.attribs.src)
     .then(image => {
       image.resize(width, height, jimp.RESIZE_BEZIER);
-      return image.getBase64Async('image/png');      
+      //image.quality(50);
+      console.log("1");
+      return image.getBase64Async('image/png');
+      //console.log("gggg " + node.attribs.src);
+      
     })
     .catch(err => {
         console.log(err);
