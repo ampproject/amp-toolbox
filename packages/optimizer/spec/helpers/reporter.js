949202275f58289467da76f3538adc73153a0b4b
Sebastian Benz <sbenz@google.com>
2018-10-30 16:00:23 +0100

    update dependencies

--- packages/optimizer/spec/helpers/reporter.js
+++ packages/optimizer/spec/helpers/reporter.js
@@ -20,7 +20,7 @@ jasmine.getEnv().clearReporters(); // remove default reporter logs
 jasmine.getEnv().addReporter(new SpecReporter({ // add jasmine-spec-reporter
   spec: {
     displayPending: true,
-    displayStacktrace: false,
+    displayStacktrace: true,
     displaySuccessful: false,
   },
   summary: {
