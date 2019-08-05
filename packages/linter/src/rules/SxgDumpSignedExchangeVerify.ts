19290596a2621e191833daae75392af190ccff7e
Michael Stillwell <mjs@beebo.org>
2019-08-06 00:15:13 +0100

    Improve dump-signedexchange tests

--- packages/linter/src/rules/SxgDumpSignedExchangeVerify.ts
+++ packages/linter/src/rules/SxgDumpSignedExchangeVerify.ts
@@ -4,12 +4,49 @@ import { fetchToCurl } from "../helper";
 import { Context } from "../index";
 import { Rule } from "../rule";
 
+function compare(
+  expected: { [k: string]: boolean | string | number },
+  actual: typeof expected
+) {
+  return Object.keys(expected).reduce(
+    (acc, k) => {
+      if (!actual[k]) {
+        acc[k] = `expected: [${expected[k]}], actual: property missing`;
+        return acc;
+      } else if (expected[k] !== actual[k]) {
+        acc[k] = `expected: [${expected[k]}], actual: [${actual[k]}]`;
+        return acc;
+      } else {
+        return acc;
+      }
+    },
+    {} as typeof expected
+  );
+}
+
+async function urlHasContentType(
+  url: string,
+  headers: Context["headers"],
+  contentType: string
+) {
+  const res = await fetch(url, { headers });
+  if (!res.ok) {
+    return false;
+  }
+  return res.headers.get("content-type") === contentType;
+}
+
+const REQUEST_HEADERS = {
+  "accept": "application/signed-exchange;v=b3",
+  "amp-cache-transform": `google;v="1"`
+};
+const EXPECTED_VERSION = "1b3";
+
 export class SxgDumpSignedExchangeVerify extends Rule {
   async run({ url, headers }: Context) {
     const opt = {
       headers: {
-        "accept": "application/signed-exchange;v=b3",
-        "amp-cache-transform": `google;v="1"`,
+        ...REQUEST_HEADERS,
         ...headers
       }
     };
@@ -17,7 +54,7 @@ export class SxgDumpSignedExchangeVerify extends Rule {
     const hdr = res.headers.get("content-type") || "";
     if (hdr.indexOf("application/signed-exchange") === -1) {
       return this.fail(
-        `[content-type: application/signed-exchange] not returned for [accept: application/signed-exchange;v=b3], [amp-cache-transform: google;v="1"] [debug: ${fetchToCurl(
+        `response is not [content-type: application/signed-exchange] [debug: ${fetchToCurl(
           url,
           opt
         )}]`
@@ -26,6 +63,7 @@ export class SxgDumpSignedExchangeVerify extends Rule {
     const body = await res.buffer();
     const CMD = `dump-signedexchange`;
     const ARGS = [`-verify`, `-json`];
+    const DEBUG = `${fetchToCurl(url, opt, false)} | ${CMD} ${ARGS.join(" ")}`;
     let sxg;
     try {
       sxg = await execa(CMD, ARGS, { input: body }).then(spawn => {
@@ -34,31 +72,65 @@ export class SxgDumpSignedExchangeVerify extends Rule {
           isValid: stdout.Valid,
           version: stdout.Version,
           uri: stdout.RequestURI,
-          status: stdout.ResponseStatus
+          status: stdout.ResponseStatus,
+          signatures: stdout.Signatures
         };
       });
     } catch (e) {
       return this.warn(
-        `not testing: couldn't execute '${CMD}' (not installed? not in PATH?)`
+        `not testing: couldn't execute [${CMD}] (not installed? not in PATH?)`
       );
     }
-    const debug = `${fetchToCurl(url, opt, false)} | ${CMD} ${ARGS.join(" ")}`;
+    const expected = {
+      isValid: true,
+      version: EXPECTED_VERSION,
+      uri: url,
+      status: 200
+    };
+    const diff = compare(expected, sxg);
+    if (Object.keys(diff).length !== 0) {
+      return this.fail(
+        `[${url}] is not valid [${JSON.stringify(diff)}] [debug: ${DEBUG}]`
+      );
+    }
+    const certUrl = sxg.signatures[0]["Params"]["cert-url"] as string;
+    if (!certUrl) {
+      return this.fail(`Can't find valid [cert-url] [${JSON.stringify(sxg)}]`);
+    }
     if (
-      !sxg.isValid ||
-      (sxg.uri !== url && sxg.version !== "1b3") ||
-      sxg.status !== 200
+      !(await urlHasContentType(
+        certUrl,
+        headers,
+        "application/cert-chain+cbor"
+      ))
     ) {
       return this.fail(
-        `[${url}] is not valid SXG [${JSON.stringify(sxg)}] [debug: ${debug}]`
+        `cert-url [${certUrl}] is not found or has wrong content type`
       );
-    } else {
-      return this.pass();
     }
+    const validityUrl = sxg.signatures[0]["Params"]["validity-url"] as string;
+    if (!validityUrl) {
+      return this.fail(`Can't find valid [cert-url] [${JSON.stringify(sxg)}]`);
+    }
+    if (!(await urlHasContentType(validityUrl, headers, "application/cbor"))) {
+      return this.fail(
+        `validity-url [${validityUrl}] is not found or has wrong content type`
+      );
+    }
+    const expires = sxg.signatures[0]["Params"]["expires"] as number;
+    if (7 * 24 * 60 * 60 + Date.now() / 1000 < expires) {
+      return this.fail(
+        `the signed content expires more than 7 days into the future [at ${new Date(
+          expires * 1000
+        )}] [debug: ${DEBUG}]`
+      );
+    }
+    return this.pass();
   }
   meta() {
     return {
       url: "",
-      title: "dump-signedexchange -verify does not report errors",
+      title: "no SXG errors found",
       info: ""
     };
   }
