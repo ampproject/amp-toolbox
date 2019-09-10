539b5eec3711d26d0abeab52cbe98cadca9f4091
Michael Stillwell <mjs@beebo.org>
2019-09-10 21:46:55 +0100

    Upgrade to Typescript 3.6.2

--- packages/linter/src/rules/EndpointsAreAccessibleFromCache.ts
+++ packages/linter/src/rules/EndpointsAreAccessibleFromCache.ts
@@ -18,8 +18,10 @@ import { isStatusOk, isAccessControlHeaders, isJson, notPass } from "../filter";
 export class EndpointsAreAccessibleFromCache extends Rule {
   async run(context: Context) {
     // Cartesian product from https://stackoverflow.com/a/43053803/11543
-    const cartesian = (a: any, b: any) =>
-      [].concat(...a.map((d: any) => b.map((e: any) => [].concat(d, e))));
+    const cartesian = (a: any[], b: any[]) =>
+      ([] as any[]).concat(
+        ...a.map((d: any) => b.map((e: any) => ([] as any[]).concat(d, e)))
+      );
     const e = corsEndpoints(context.$);
     const product = cartesian(e, (await caches()).map(c => c.cacheDomain));
     const canXhrCache = async (xhrUrl: string, cacheSuffix: string) => {
