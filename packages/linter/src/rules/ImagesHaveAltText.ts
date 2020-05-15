46091224faed2c666abac24f8d9d74591013e520
Damani Brown <damanibrown@google.com>
2020-05-15 16:38:51 -0400

    Changed image alt text to output a more readable format

--- packages/linter/src/rules/ImagesHaveAltText.ts
+++ packages/linter/src/rules/ImagesHaveAltText.ts
@@ -3,16 +3,31 @@ import { Rule } from "../rule";
 
 export class ImagesHaveAltText extends Rule {
   run({ $ }: Context) {
-    let imgsWithoutAlt = "";
+    let imgsWithoutAlt: { [key: string]: number } = {};
 
     $("amp-img").each(function (i, elem) {
       if (!elem.attribs.alt) {
-        imgsWithoutAlt = imgsWithoutAlt + "- " + elem.attribs.src + "\n";
+        if (typeof imgsWithoutAlt[elem.attribs.src] == "undefined") {
+          imgsWithoutAlt[elem.attribs.src] = 1;
+        } else {
+          imgsWithoutAlt[elem.attribs.src] += 1;
+        }
       }
     });
 
-    return imgsWithoutAlt.length > 0
-      ? this.warn(`Missing alt text from images: \n` + imgsWithoutAlt)
+    let output = "";
+    let m = `${JSON.stringify(imgsWithoutAlt)}`.split(",");
+    m.forEach(function (el) {
+      output +=
+        el
+          .replace('{"', "")
+          .replace('":', " (found ")
+          .replace('"', "")
+          .replace("}", "") + " times)\n";
+    });
+
+    return Object.keys(imgsWithoutAlt).length > 0
+      ? this.warn("Missing alt text from images: \n" + output)
       : this.pass();
   }
   meta() {
