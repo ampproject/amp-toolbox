bf08d062afc19be3833cf9fdf3b3b27e7c713484
Damani <damanibrown@google.com>
2020-05-20 13:30:18 -0400

    Condense linter output for image alt text check (#780)
    * Changed image alt text to output a more readable format
    
    * Removed two unnecessary tests from the linter Story mode
    
    * Refactored string building code
    
    * Removed usage count for single images
    
    * Quick fix for Story attribute output
    
    * formatting for e2e

--- packages/linter/src/rules/ImagesHaveAltText.ts
+++ packages/linter/src/rules/ImagesHaveAltText.ts
@@ -3,16 +3,27 @@ import { Rule } from "../rule";
 
 export class ImagesHaveAltText extends Rule {
   run({ $ }: Context) {
-    let imgsWithoutAlt = "";
+    let imgsWithoutAlt: { [key: string]: number } = {};
+    let output = "";
 
     $("amp-img").each(function (i, elem) {
       if (!elem.attribs.alt) {
-        imgsWithoutAlt = imgsWithoutAlt + "- " + elem.attribs.src + "\n";
+        if (typeof imgsWithoutAlt[elem.attribs.src] == "undefined") {
+          imgsWithoutAlt[elem.attribs.src] = 1;
+        } else {
+          imgsWithoutAlt[elem.attribs.src] += 1;
+        }
       }
     });
 
-    return imgsWithoutAlt.length > 0
-      ? this.warn(`Missing alt text from images: \n` + imgsWithoutAlt)
+    for (let key in imgsWithoutAlt) {
+      imgsWithoutAlt[key] > 1
+        ? (output += key + " (used " + imgsWithoutAlt[key] + " times)\n")
+        : (output += key + "\n");
+    }
+
+    return Object.keys(imgsWithoutAlt).length > 0
+      ? this.warn("Missing alt text from images: \n" + output)
       : this.pass();
   }
   meta() {
