2cc4aa84284e6dde10af75d4b17279e25904b87f
Sebastian Benz <sebastian.benz@gmail.com>
2020-08-03 13:58:01 +0200

    Enable amp-img => img transformation for hero images (#880)
    * Enable amp-img => img transformation for hero images
    
    * update readme
    
    * mark generated hero images
    
    * Improve readme
    
    Co-authored-by: Weston Ruter <westonruter@google.com>
    
    Co-authored-by: Weston Ruter <westonruter@google.com>

--- packages/optimizer/lib/transformers/PreloadHeroImage.js
+++ packages/optimizer/lib/transformers/PreloadHeroImage.js
@@ -51,7 +51,6 @@ class PreloadHeroImage {
   constructor(config) {
     this.log = config.log;
     this.enabled = config.preloadHeroImage !== false;
-    this.experimentImg = config.experimentImg;
   }
 
   async transform(root, params) {
@@ -293,9 +292,6 @@ class PreloadHeroImage {
   }
 
   generateImg(node) {
-    if (!this.experimentImg) {
-      return;
-    }
     if (!node) {
       return;
     }
@@ -320,6 +316,7 @@ class PreloadHeroImage {
       }
     }
     node.attribs['i-amphtml-ssr'] = '';
+    node.attribs['data-hero'] = '';
     appendChild(node, imgNode);
   }
 }
