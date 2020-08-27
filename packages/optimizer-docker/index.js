36a953d5f00288f43ecc6c7167ceed225ee58eb7
Jake Fried <samouri@users.noreply.github.com>
2020-08-27 11:29:06 -0400

    remove express and use nodejs directly

--- packages/optimizer-docker/index.js
+++ packages/optimizer-docker/index.js
@@ -1,85 +1,67 @@
 import http from 'http';
-import AmpOptimizer from "@ampproject/toolbox-optimizer";
-const ampOptimizer = AmpOptimizer.create(getOptions());
-
+import url from 'url'
+import AmpOptimizer from '@ampproject/toolbox-optimizer';
+const ampOptimizer = AmpOptimizer.create(getStaticOptions());
 
 const port = 3000;
 
-const opts = {};
-
-// TODO: this needs to be set per request.
-// Do we want this at all? Probably means accepting json body vs. html body.
-if (process.env.CANONICAL) {
-  opts.canonical = process.env.CANONICAL;
-}
-
-async function parseBody(req) {
+async function parseRequest(req) {
   return new Promise((resolve, reject) => {
     let data = [];
 
-    req.on('data', chunk => {
-      data.push(chunk)
-    }) 
-    req.on('error', (err) => reject(err)); 
-    req.on('end', () => resolve(data)); 
-  })
+    req.on('data', (chunk) => {
+      data.push(chunk);
+    });
+    req.on('error', (err) => reject(err));
+    req.on('end', () =>
+      resolve({
+        body: data.Body.toString(),
+        query: url.parse(req.url,true).query,
+      })
+    );
+  });
 }
 
-const server = http.createServer((req, res) => {
-  const originalHtml = req.body;
-  if (!req.body) {
-    res.status = 400;
-    res.send("Error: please provide html in the body of your post request.");
+const server = http.createServer(async (req, res) => {
+  const {body} = await parseRequest(req);
+  if (!body) {
+    res.writeHead(400);
+    res.end('Error: please provide html in the body of your post request.');
     return;
   }
 
   ampOptimizer
     .transformHtml(originalHtml, opts)
-    .then(optimizedHtml => {
-      res.set("Content-Type", "text/html");
-      res.send(optimizedHtml);
+    .then((optimizedHtml) => {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(optimizedHtml);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
 
-      res.status = 500;
-      res.set("Content-Type", "text/plain");
-      res.send(`Error: ${err.message}`);
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end(optimizedHtml);
+      res.send('500: Internal Service Error.');
     });
 });
 
 server.listen(port);
 console.log(`AMP Optimizer listening at http://localhost:${port}`);
 
-function camelToSnakeCase(str) {
-  return str.replace(/[A-Z]/g, letter => `_${letter}`).toUpperCase();
+function snakeToCamel(str) {
+  return str.toLowerCase().replace(/_[a-z]/g, (letter) => `_${letter.toUpperCase()}`);
 }
 
-function getOptions() {
-  const opts = {};
-  [
-    "autoAddMandatoryTags", // defaults true
-    "autoExtensionImport", // default true
-    "format", // default AMP
-    "fetch", // TODO: remove, cannot specify function.
-    "experimentEsm",
-    "imageBasePath", // default undefined. valid arg is a string.
-    "imageOptimizer", // TODO: remove, cannot specify function.
-    "lts", // default false
-    "markdown", // default false
-    "minify", // default true
-    "preloadHeroImage", // default true
-    "verbose" // default false
-  ].forEach(option => {
-    const snakeCaseOpt = camelToSnakeCase(option);
-    if (snakeCaseOpt in process.env) {
-      opts[option] = process.env[snakeCaseOpt];
-    }
-  });
-
-  return opts;
+function getStaticOptions() {
+  const optionEntries = Object.keys(process.env)
+    .filter((envVar) => envVar.startsWith('AMP_OPTIMIZER_'))
+    .map((envVar) => {
+      const optimizerFlag = snakeToCamel(envVar.substring('AMP_OPTIMIZER'.length));
+      return [optimizerFlag, process.env[envVar]];
+    });
+  return Object.fromEntries(optionEntries);
 }
 
-process.on("SIGINT", function() {
+process.on('SIGINT', function () {
   process.exit();
 });
