bf578298ba2d3e92ee9042d8bd3461ee5b0a969a
Kevin Kimball <kwkimball@gmail.com>
2020-12-02 13:31:04 -0800

    Add check for amp-img placeholder does not make additional request (#962)
    * Add check for amp-img placeholder does not make addl request
    
    * Hook up test to index

---
+++ packages/linter/src/rules/AmpImgPlaceholderIsDataUri.ts
@@ -0,0 +1,36 @@
+import { Context, Result } from "../index";
+import { Rule } from "../rule";
+import { notPass } from '../filter';
+
+export class AmpImgPlaceholderIsDataUri extends Rule {
+  async run(context: Context) {
+    const $ = context.$;
+
+    const incorrectImages = $("amp-img")
+      .filter((_, e) => {
+        const placeholders = $(e).children('amp-img[placeholder]');
+        const hasIncorrectPlaceholder = !!placeholders.toArray().find((placeholder) => 
+          !$(placeholder).attr("src").startsWith('data:')
+        );
+        return hasIncorrectPlaceholder;
+      });
+
+    return (
+      await Promise.all(
+        incorrectImages.map((_, e) => {
+          const s = $(e).toString();
+          return this.warn(
+            `[${s}] has a placeholder that makes a network request. This hurts LCP, omit it or use a data URI.`
+          );
+        }).get() as Array<Promise<Result>>
+      )
+    ).filter(notPass);
+  }
+  meta() {
+    return {
+      url: "",
+      title: "<amp-img> placeholder should not make a network request",
+      info: "",
+    };
+  }
+}
