923b6228701499c2c2fc58b3d594a039e4f676fb
Jake Fried <samouri@users.noreply.github.com>
2021-04-28 09:59:33 -0400

    This commit should have a failing CI

--- packages/cloudflare-optimizer-scripts/test/index.test.js
+++ packages/cloudflare-optimizer-scripts/test/index.test.js
@@ -106,6 +106,7 @@ describe('handleEvent', () => {
   });
 
   it('Should passthrough request to origin in request interceptor mode', async () => {
+    throw new Error('Please fail.');
     const input = `<html amp><body></body></html>`;
     global.fetch.mockReturnValue(getResponse(input));
     await getOutput('http://test.com');
